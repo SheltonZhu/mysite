34f880cc3e37d4852391dac69438a92fb58b4fc0
SheltonZhu <498220739@qq.com>
2021-04-10 03:00:51 +0800

    update config

--- middlewares/csrf.go
+++ middlewares/csrf.go
@@ -3,19 +3,20 @@ package middlewares
 import (
 	"github.com/gin-gonic/gin"
 	csrf "github.com/utrack/gin-csrf"
+	"os"
 )
 
 func Csrf() gin.HandlerFunc {
 	return csrf.Middleware(csrf.Options{
-		Secret:        "cool_csrf_secret",
+		Secret: os.Getenv("CSRF_TOKEN_SECRET"),
 		//IgnoreMethods: []string{"HEAD", "OPTIONS"},
 		ErrorFunc: func(c *gin.Context) {
 			c.String(400, "CSRF token mismatch")
 			c.Abort()
 		},
 		//default: X-CSRF-TOKEN
-		//TokenGetter: func(c *gin.Context) string {
-		//	return c.Request.Header.Get("csrftoken")
-		//},
+		TokenGetter: func(c *gin.Context) string {
+			return c.Request.Header.Get("X-CSRF-TOKEN")
+		},
 	})
 }
--- middlewares/session.go
+++ middlewares/session.go
@@ -4,11 +4,12 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func Session() gin.HandlerFunc {
 	//"github.com/gin-contrib/sessions/redis"
 	//store, _ := redis.NewStore(10, "tcp", "localhost:6379", "", []byte("secret"))
-	store := cookie.NewStore([]byte("cool_secret"))
-	return sessions.Sessions("cool_session", store)
+	store := cookie.NewStore([]byte(os.Getenv("SESSION_SECRET")))
+	return sessions.Sessions(os.Getenv("SESSION_NAME"), store)
 }
